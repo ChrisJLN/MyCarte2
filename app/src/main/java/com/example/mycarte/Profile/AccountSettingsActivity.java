8bf12f336825c1bc84a7f9181a7ba3f3e5c6ab54
ChrisJLN <lenhutchris@gmail.com>
2019-04-04 12:19:46 -0500

    Profile Update Function

---
+++ app/src/main/java/com/example/mycarte/Profile/AccountSettingsActivity.java
@@ -0,0 +1,18 @@
+package com.example.mycarte.Profile;
+
+import android.nfc.Tag;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class AccountSettingsActivity extends AppCompatActivity
+{
+    private static final String TAG = "AccountSettingsActivity";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accountsettings);
+        Log.d(TAG, "onCreate: started.");
+    }
+}
--- app/src/main/java/com/example/mycarte/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/mycarte/Profile/ProfileActivity.java
@@ -4,54 +4,53 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-//import android.widget.Toolbar;
 
 import com.example.mycarte.R;
 import com.example.mycarte.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class ProfileActivity extends AppCompatActivity
-{
+public class ProfileActivity extends AppCompatActivity{
     private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
 
     private Context mContext = ProfileActivity.this;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
+
         setupBottomNavigationView();
-        //setupToolbar();
+        setupToolbar();
     }
 
-    /**private void setupToolbar()
+    private void setupToolbar()
     {
-        Toolbar toolbar = (Toolbar)findViewById(R.id.profileToolBar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener()
-        {
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item)
-            {
+            public boolean onMenuItemClick(MenuItem item) {
                 Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
-                switch (item.getItemId())
-                {
+
+                switch (item.getItemId()){
                     case R.id.profileMenu:
-                    Log.d(TAG, "onMenuItemClick: Navigating to Profile Preferences.");
+                        Log.d(TAG, "onMenuItemClick: Navigating to Profile Preferences.");
                 }
+
                 return false;
             }
         });
     }
+
     /**
-     * Bottom NavigationView Setup
+     * BottomNavigationView setup
      */
     private void setupBottomNavigationView()
     {
@@ -64,4 +63,9 @@ public class ProfileActivity extends AppCompatActivity
         menuItem.setChecked(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
 }
