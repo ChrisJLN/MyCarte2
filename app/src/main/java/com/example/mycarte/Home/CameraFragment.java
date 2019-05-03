b648681d41073084daa714658ea0bd3beec2eecb
ChrisJLN <lenhutchris@gmail.com>
2019-05-02 21:02:01 -0500

    Google Map Front

--- app/src/main/java/com/example/mycarte/Home/CameraFragment.java
+++ app/src/main/java/com/example/mycarte/Home/CameraFragment.java
@@ -18,7 +18,7 @@ public class CameraFragment extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        View view = inflater.inflate(R.layout.activity_maps, container, false);
         return view;
     }
 
