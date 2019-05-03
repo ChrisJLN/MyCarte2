15f6ffc258735985dfc63c180641498b1e7d1d52
ChrisJLN <lenhutchris@gmail.com>
2019-05-02 23:42:58 -0500

    Google Map Front

--- app/src/main/java/com/example/mycarte/Likes/LikesActivity.java
+++ app/src/main/java/com/example/mycarte/Likes/LikesActivity.java
@@ -1,44 +1,79 @@
 package com.example.mycarte.Likes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.mycarte.MapsActivity;
 import com.example.mycarte.R;
+import com.example.mycarte.UserInformation;
 import com.example.mycarte.Utils.BottomNavigationViewHelper;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class LikesActivity extends AppCompatActivity
+public class LikesActivity extends AppCompatActivity implements View.OnClickListener
 {
     private static final String TAG = "LikesActivity";
     private static final int ACTIVITY_NUM = 3;
     private Context mContext = LikesActivity.this;
+    private DatabaseReference mDatabase;
+    private Button btnsave;
+    private Button btnproceed;
+    private EditText editTextName;
+    private EditText editTextLatitude;
+    private EditText editTextLongitude;
+
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Log.d(TAG, "onCreate: started.");
-        setupBottomNavigationView();
+        setContentView(R.layout.activity_likes);
+        btnproceed=(Button)findViewById(R.id.btnproceed);
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("photos");
+        editTextName=(EditText)findViewById(R.id.editTextName);
+        editTextLatitude=(EditText)findViewById(R.id.editTextLatitude);
+        editTextLongitude=(EditText)findViewById(R.id.editTextLongitude);
+        btnsave=(Button)findViewById(R.id.btnsave);
+        btnsave.setOnClickListener(this);
+        btnproceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(LikesActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
-
-    /**
-     * Bottom NavigationView Setup
-     */
-    private void setupBottomNavigationView()
-    {
-        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
-        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
+    private void saveUserInformation(){
+        String name =editTextName.getText().toString().trim();
+        double latitude= Double.parseDouble(editTextLatitude.getText().toString().trim());
+        double longitude= Double.parseDouble(editTextLongitude.getText().toString().trim());
+        UserInformation userInformation=new UserInformation(name,latitude,longitude);
+        mDatabase.child("photos").setValue(userInformation);
+        Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show();
     }
-
+    @Override
+    public void onClick(View view) {
+        if(view==btnproceed){
+            finish();
+        }
+        if (view==btnsave){
+            saveUserInformation();
+            editTextName.getText().clear();
+            editTextLatitude.getText().clear();
+            editTextLongitude.getText().clear();
+        }
+    }
 }
